81e23a2a01e1fc61afb2eda6bf3a47b5ed7294a9
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-07-08 21:33:55 +0900

    Implement an algorithm to calculate the purity using an enhanced suffix array.
    esaxx-0.0.1 is used to generate an enhanced suffix array.
    Its newer versions 0.0.2 and 0.0.3 have bugs at this time.

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #include "pstade/oven/utf8_decoded.hpp"
 #include "pstade/oven/utf8_encoded.hpp"
 #include "cmdline.h"
-#include "sais.hxx"
+#include "esa.hxx"
 
 
 using namespace std;
@@ -57,6 +57,7 @@ int main(int argc, char* argv[]) {
     cmdline::parser p;
     p.add("help", 'h', "");
     p.add<string>("mode", 'm', "", false, "binary", cmdline::oneof<string>("binary", "text"));
+    p.add("show-substring", 's', "");
     if (!p.parse(argc, argv) || p.exist("help")) {
         cout << p.error_full() << p.usage();
         return 0;
@@ -83,17 +84,94 @@ int main(int argc, char* argv[]) {
 
         // suffix array
         vector<index_type> sa(input.size());
-        int err = saisxx(input.begin(),
-                         sa.begin(),
-                         static_cast<index_type>(input.size()),
-                         static_cast<index_type>(alphabet_size));
+        vector<index_type>  l(input.size());
+        vector<index_type>  r(input.size());
+        vector<index_type>  d(input.size());
+        index_type  num_nodes;
+        int err = esaxx(input.begin(),
+                        sa.begin(),
+                        l.begin(), r.begin(), d.begin(),
+                        static_cast<index_type>(input.size()),
+                        static_cast<index_type>(alphabet_size),
+                        num_nodes);
         if (err) throw runtime_error("saisxx failed to construct a suffix array.");
 
-        // output suffixes
-        for (auto i : sa) {
-            auto suffix = oven::make_range(input.begin() + i, input.end());
-            oven::copy(suffix,
-                       ostream_iterator<byte_type>(std::cout, ""));
+        // suffix_to_parent_node[k]: 接尾辞input[k..$]に対応する葉ノードの、親ノードのpost-order順の番号。
+        // post-order巡回により、直接の親が最初に値を設定する（最初かどうかは-1かどうかで判定する）。
+        vector<index_type> suffix_to_parent_node(input.size(), -1);
+        for (int i = 0; i < num_nodes; ++i) {
+            // ノードi直下の全ての葉ノードjについて、接尾辞input[k..$]からノードiへのリンクを張る
+            for (int j = l[i]; j < r[i]; ++j) {
+                const auto k = sa[j];
+                if (suffix_to_parent_node[k] < 0) {
+                    suffix_to_parent_node[k] = i;
+                }
+            }
+        }
+
+        // process all internal nodes
+        for (int i = 0; i < num_nodes - 1; ++i) {  // ルート以外のノードをpost-order巡回する。
+            // このループではノードi（iはpost-orderでの番号）に対応する部分文字列substrを扱う。
+
+            // 内部ノードiに対応する部分文字列の出現頻度と長さ。
+            const auto freq_substr = r[i] - l[i];
+            const auto len_substr  = d[i];
+
+            // substrと同じ出現回数のsub-substrを数える。
+            int count = 0;
+            {
+                // ノードiの親ノードjを見つける。
+                auto j = i + 1;
+                while (!(d[j] < d[i] && l[j] <= l[i] && r[i] <= r[j])) ++j;
+
+                // substrの末尾を0文字以上削って得られるsub-substrの内で、出現
+                // 回数がsubstrと同じものの数はd[i] - d[j]である。
+                count += d[i] - d[j];
+            }
+            for (int j = 1; j < d[i]; ++j) {
+                // substrの先頭をj文字削ったsub-substrを考える。
+
+                // sub-substrに対応するノードを見つける。
+                auto k = suffix_to_parent_node[sa[l[i]] + j];  // 接尾辞input[(sa[l[i]] + j)..$]に対応する葉ノードの親ノード
+                const auto len_subsubstr = d[i] - j;
+                while (d[k] > len_subsubstr) ++k;  // d[k] == len_subsubstr ならば、ノードkはsub-substrに対応するノード。
+
+                if (d[k] < len_subsubstr) {
+                    // このsub-substrは1回しか出現していない。
+                    // 今考えているsubstrは内部ノードに対応しており、出現頻度が
+                    // 2以上なので、purityを考える場合は、このsub-substrを無視
+                    // してよい。
+                }
+                else {
+                    // このsub-substrは2回以上出現している。
+                    const auto freq_subsubstr = r[k] - l[k];
+                    if (freq_subsubstr == freq_substr) {
+                        // ノードkの親ノードmを見つける。
+                        auto m = k + 1;
+                        while (!(d[m] < d[k] && l[m] <= l[k] && r[k] <= r[m])) ++m;
+
+                        // sub-substrの末尾を0文字以上削って得られる
+                        // sub-sub-substrの内で、出現回数がsub-substrと同じもの
+                        // の数はd[k] - d[m]である。
+                        count += d[k] - d[m];
+                    }
+                }
+            }
+
+            // purity of substr
+            const int num_subsubstrs = (1 + len_substr) * len_substr / 2;
+            const double purity = static_cast<double>(count) / num_subsubstrs;
+
+            // 出力
+            const auto pos = sa[l[i]];
+            std::cout << pos << "\t" << len_substr << "\t" << freq_substr << "\t" << purity;
+            if (p.exist("show-substring")) {
+                auto first = input.begin() + sa[l[i]];
+                auto substr = oven::make_range(first, first + len_substr);
+                std::cout << "\t";
+                oven::copy(substr, ostream_iterator<byte_type>(std::cout, ""));
+            }
+            std::cout << "\n";
         }
     }
     else {
