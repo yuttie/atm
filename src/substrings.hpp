55e71d660f49e357eb8ca6d498555f8b4ed09a30
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-10-11 11:20:22 +0900

    Re-add the former Substrings class as BranchingSubstrings class.

--- src/substrings.hpp
+++ src/substrings.hpp
@@ -9,6 +9,236 @@
 
 
 template <class Char, class Index>
+struct BranchingSubstrings {
+    typedef Index index_type;
+    struct substr {
+        typedef typename std::vector<Char>::const_iterator iterator;
+        typedef typename std::vector<Char>::const_iterator const_iterator;
+
+        index_type pos()       const { return parent_->sa_[parent_->l_[i_]]; }
+        index_type length()    const { return parent_->d_[i_]; }
+        index_type frequency() const { return parent_->r_[i_] - parent_->l_[i_]; }
+        double     spurity()   const { return parent_->strict_purity(i_); }
+        double     lpurity()   const { return parent_->loose_purity(i_); }
+
+        iterator begin() const {
+            return parent_->input_.begin() + pos();
+        }
+
+        iterator end() const {
+            return parent_->input_.begin() + pos() + length();
+        }
+
+        substr(const BranchingSubstrings* parent, int i)
+            : parent_(parent), i_(i)
+        {}
+
+    private:
+        const BranchingSubstrings* parent_;
+        int i_;
+    };
+
+private:
+    template <class Value>
+    struct substring_iterator
+        : public boost::iterator_facade<
+            substring_iterator<Value>,
+            Value,
+            boost::random_access_traversal_tag,
+            Value,
+            int>
+    {
+        substring_iterator()
+            : parent_(0), i_(-1)
+        {}
+
+        substring_iterator(const BranchingSubstrings* parent, int i)
+            : parent_(parent), i_(i)
+        {}
+
+    private:
+        friend class boost::iterator_core_access;
+
+        void increment() { ++i_; }
+
+        void decrement() { --i_; }
+
+        void advance(int n) { i_ += n; }
+
+        int distance_to(const substring_iterator<Value>& other) const { return other.i_ - this->i_; }
+
+        bool equal(const substring_iterator<Value>& other) const {
+            return this->parent_ == other.parent_ && this->i_ == other.i_;
+        }
+
+        Value dereference() const {
+            return substr(parent_, i_);
+        }
+
+        const BranchingSubstrings* parent_;
+        int i_;
+    };
+
+public:
+    typedef substring_iterator<substr> iterator;
+    typedef substring_iterator<const substr> const_iterator;
+
+    BranchingSubstrings(const std::vector<Char>& input, const size_t alphabet_size)
+        : input_(input),
+          sa_(input.size()),
+          l_(input.size()),
+          r_(input.size()),
+          d_(input.size()),
+          suffix_to_parent_node_(input.size(), -1),
+          node_to_parent_node_()
+    {
+        // suffix array
+        int err = esaxx(input_.begin(),
+                        sa_.begin(),
+                        l_.begin(), r_.begin(), d_.begin(),
+                        static_cast<index_type>(input_.size()),
+                        static_cast<index_type>(alphabet_size),
+                        num_nodes_);
+        if (err) throw std::runtime_error("saisxx failed to construct a suffix array.");
+
+        // suffix_to_parent_node[k]: 接尾辞input[k..$]に対応する葉ノードの、親ノードのpost-order順の番号。
+        // post-order巡回により、直接の親が最初に値を設定する（最初かどうかは-1かどうかで判定する）。
+        for (int i = 0; i < num_nodes_; ++i) {
+            // ノードi直下の全ての葉ノードjについて、接尾辞input[k..$]からノードiへのリンクを張る
+            for (int j = l_[i]; j < r_[i]; ++j) {
+                const auto k = sa_[j];
+                if (suffix_to_parent_node_[k] < 0) {
+                    suffix_to_parent_node_[k] = i;
+                }
+            }
+        }
+
+        // node_to_parent_node[i]: ノードiの親ノードの番号（post-order）。
+        node_to_parent_node_.resize(num_nodes_);
+        std::stack<index_type> stk;
+        stk.push(num_nodes_ - 1);
+        for (int i = num_nodes_ - 2; i >= 0; --i) {
+            while (!(l_[stk.top()] <= l_[i] && r_[i] <= r_[stk.top()])) {
+                stk.pop();
+            }
+            node_to_parent_node_[i] = stk.top();
+            stk.push(i);
+        }
+    }
+
+    iterator begin() const {
+        return iterator(this, 0);
+    }
+
+    iterator end() const {
+        return iterator(this, num_nodes_ - 1);
+    }
+
+private:
+    double strict_purity(const int i) const {
+        // ここではノードi（iはpost-orderでの番号）に対応する部分文字列substrを扱う。
+        const auto freq_substr = r_[i] - l_[i];
+        const auto len_substr  = d_[i];
+        const auto pos_substr  = sa_[l_[i]];
+
+        // substrと同じ出現回数のsub-substrを数える。
+        int count = 0;
+        {
+            // substrの末尾を0文字以上削って得られるsub-substrについて考える。
+            // ノードiに対応する部分文字列をsubstr[i]とすると、substr[i]の末尾
+            // を削って得られる部分文字列の内で、substr[i]と同じ頻度をもつもの
+            // はsuffix tree上ではノードiにまとめられている
+            // （分岐が無い <=> 頻度が同じ）。
+
+            // ノードiの親ノードjを見つける。
+            const auto j = node_to_parent_node_[i];
+
+            // substrの末尾を0文字以上削って得られるsub-substrの内で、出現
+            // 回数がsubstrと同じものの数はd[i] - d[j]である。
+            count += d_[i] - d_[j];
+        }
+        for (int j = 1; j < len_substr; ++j) {
+            // substrの先頭をj文字削ったsub-substrを考える。
+            const auto len_subsubstr = len_substr - j;
+
+            // sub-substrに対応するノードを見つける。
+            // {内部,葉}ノードに対応する部分文字列から先頭の1文字を削って得られ
+            // る文字列には、必ず対応する{内部,葉}ノードが存在する。
+            auto k = suffix_to_parent_node_[pos_substr + j];  // 接尾辞input[(pos_substr + j)..$]に対応する葉ノードの親ノード
+            while (d_[k] > len_subsubstr) k = node_to_parent_node_[k];  // d[k] == len_subsubstr ならば、ノードkはsub-substrに対応するノード。
+
+            // sub-substrの出現回数をチェックする。
+            const auto freq_subsubstr = r_[k] - l_[k];
+            if (freq_subsubstr == freq_substr) {
+                // ノードkの親ノードmを見つける。
+                const auto m = node_to_parent_node_[k];
+
+                // sub-substrの末尾を0文字以上削って得られる
+                // sub-sub-substrの内で、出現回数がsub-substrと同じもの
+                // の数はd[k] - d[m]である。
+                count += d_[k] - d_[m];
+            }
+        }
+
+        // strict purity of substr
+        const int num_subsubstrs = (1 + len_substr) * len_substr / 2;
+        const double spurity = static_cast<double>(count) / num_subsubstrs;
+
+        return spurity;
+    }
+
+    double loose_purity(const int i) const {
+        // ここではノードi（iはpost-orderでの番号）に対応する部分文字列substrを扱う。
+        const auto freq_substr = r_[i] - l_[i];
+        const auto len_substr  = d_[i];
+        const auto pos_substr  = sa_[l_[i]];
+
+        double support = 0;
+        {
+            // substrの末尾を0文字以上削って得られるsub-substrについて考える。
+            for (index_type j = i, k = node_to_parent_node_[i]; d_[j] > 0; j = k, k = node_to_parent_node_[k]) {
+                const auto num_subsubstrs_of_same_frequency = d_[j] - d_[k];
+                const auto freq_subsubstr = r_[j] - l_[j];
+                const double sup = static_cast<double>(freq_substr) / freq_subsubstr;
+                support += num_subsubstrs_of_same_frequency * sup;
+            }
+        }
+        for (int j = 1; j < len_substr; ++j) {
+            // substrの先頭をj文字削ったsub-substrを考える。
+            const auto len_subsubstr = len_substr - j;
+
+            // sub-substrに対応するノードを見つける。
+            auto k = suffix_to_parent_node_[pos_substr + j];  // 接尾辞input[(pos_substr + j)..$]に対応する葉ノードの親ノード
+            while (d_[k] > len_subsubstr) k = node_to_parent_node_[k];  // d[k] == len_subsubstr ならば、ノードkはsub-substrに対応するノード。
+
+            // sub-substrの末尾を0文字以上削って得られるsub-substrについて考える。
+            for (index_type m = k, n = node_to_parent_node_[k]; d_[m] > 0; m = n, n = node_to_parent_node_[n]) {
+                const auto num_subsubstrs_of_same_frequency = d_[m] - d_[n];
+                const auto freq_subsubstr = r_[m] - l_[m];
+                const double sup = static_cast<double>(freq_substr) / freq_subsubstr;
+                support += num_subsubstrs_of_same_frequency * sup;
+            }
+        }
+
+        // loose purity of substr
+        const int num_subsubstrs = (1 + len_substr) * len_substr / 2;
+        const double lpurity = support / num_subsubstrs;
+
+        return lpurity;
+    }
+
+    const std::vector<Char>& input_;
+    std::vector<index_type> sa_;
+    std::vector<index_type>  l_;
+    std::vector<index_type>  r_;
+    std::vector<index_type>  d_;
+    index_type  num_nodes_;
+    std::vector<index_type> suffix_to_parent_node_;
+    std::vector<index_type> node_to_parent_node_;
+};
+
+
+template <class Char, class Index>
 struct Substrings {
     typedef Index index_type;
     struct substr {
