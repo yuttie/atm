226761125cbfdb85aedb04a0928703c639cf1d96
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-12-24 23:05:40 +0900

    Add a new enumerator "Segments".

--- src/substrings.hpp
+++ src/substrings.hpp
@@ -1399,9 +1399,31 @@ public:
         return const_iterator(this, n_, 0, 0);
     }
 
-private:
+protected:
     using base_type::input_;
     const int n_;
+};
+
+
+template <class Char, class Index>
+struct Segments : public CoarseSubstrings<Char, Index> {
+    typedef CoarseSubstrings<Char, Index> base_type;
+    using typename base_type::index_type;
+    using typename base_type::substr;
+    using typename base_type::iterator;
+    using typename base_type::const_iterator;
+
+    Segments(const std::vector<Char>& input, const size_t alphabet_size, const int n)
+        : base_type(input, alphabet_size, n)
+    {}
+
+    iterator begin() {
+        return iterator(this, base_type::n_, 0, 1);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(this, base_type::n_, 0, 1);
+    }
 };
 
 
