2002c149109d7c26aeacd651b2546408efa0903c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-12-25 12:41:14 +0900

    Add a new enumerator "NGrams".

--- src/substrings.hpp
+++ src/substrings.hpp
@@ -1427,4 +1427,37 @@ struct Segments : public CoarseSubstrings<Char, Index> {
 };
 
 
+template <class Char, class Index>
+struct NGrams : public SubstringsFromLongest<Char, Index> {
+    typedef SubstringsFromLongest<Char, Index> base_type;
+    using typename base_type::index_type;
+    using typename base_type::substr;
+    using typename base_type::iterator;
+    using typename base_type::const_iterator;
+
+    NGrams(const std::vector<Char>& input, const size_t alphabet_size, const int n)
+        : base_type(input, alphabet_size), n_(n)
+    {}
+
+    iterator begin() {
+        return iterator(this, 0, n_);
+    }
+
+    iterator end() {
+        return iterator(this, 0, n_ - 1);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(this, 0, n_);
+    }
+
+    const_iterator end() const {
+        return const_iterator(this, 0, n_ - 1);
+    }
+
+private:
+    const int n_;
+};
+
+
 #endif  /* SUBSTRINGS_HPP */
