49cb52340ed95a0c16aeba02d20c598238ece73e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-12-26 00:28:13 +0900

    Add a new enumerator CoarseNGrams.

--- src/substrings.hpp
+++ src/substrings.hpp
@@ -1460,4 +1460,38 @@ private:
 };
 
 
+template <class Char, class Index>
+struct CoarseNGrams : public CoarseSubstrings<Char, Index> {
+    typedef CoarseSubstrings<Char, Index> base_type;
+    using typename base_type::index_type;
+    using typename base_type::substr;
+    using typename base_type::iterator;
+    using typename base_type::const_iterator;
+
+    CoarseNGrams(const std::vector<Char>& input, const size_t alphabet_size, const int r, const int n)
+        : base_type(input, alphabet_size, r), r_(r), n_(n)
+    {}
+
+    iterator begin() {
+        return iterator(this, r_, 0, n_);
+    }
+
+    iterator end() {
+        return iterator(this, r_, 0, n_ - 1);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(this, r_, 0, n_);
+    }
+
+    const_iterator end() const {
+        return const_iterator(this, r_, 0, n_ - 1);
+    }
+
+private:
+    const int r_;
+    const int n_;
+};
+
+
 #endif  /* SUBSTRINGS_HPP */
