d9062eee7661e8f2d67986d1e95650fdba2ac4e1
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-12-26 21:22:22 +0900

    Add an enumerator SingleRange that only has a single substring of a given range.

--- src/substrings.hpp
+++ src/substrings.hpp
@@ -1494,4 +1494,38 @@ private:
 };
 
 
+template <class Char, class Index>
+struct SingleRange : public SubstringsFromLongest<Char, Index> {
+    typedef SubstringsFromLongest<Char, Index> base_type;
+    using typename base_type::index_type;
+    using typename base_type::substr;
+    using typename base_type::iterator;
+    using typename base_type::const_iterator;
+
+    SingleRange(const std::vector<Char>& input, const size_t alphabet_size, const int i, const int j)
+        : base_type(input, alphabet_size), i_(i), j_(j)
+    {}
+
+    iterator begin() {
+        return iterator(this, i_, j_);
+    }
+
+    iterator end() {
+        return begin() + 1;
+    }
+
+    const_iterator begin() const {
+        return const_iterator(this, i_, j_);
+    }
+
+    const_iterator end() const {
+        return begin() + 1;
+    }
+
+private:
+    const int i_;
+    const int j_;
+};
+
+
 #endif  /* SUBSTRINGS_HPP */
