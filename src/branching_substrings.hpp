4e720994ebcfd0f9d08e4b0c7fcf947b5753724c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-07-28 06:09:56 +0900

    Add 'core' class based on the BranchingSubstrings class.

--- src/branching_substrings.hpp
+++ src/branching_substrings.hpp
@@ -11,6 +11,164 @@
 
 
 template <class Char, class Index>
+struct core {
+    typedef Index index_type;
+    struct substr {
+        typedef typename std::vector<Char>::const_iterator iterator;
+        typedef typename std::vector<Char>::const_iterator const_iterator;
+
+        index_type pos()       const { return parent_->sa_[parent_->l_[i_]]; }
+        index_type length()    const { return parent_->d_[i_]; }
+        index_type frequency() const { return parent_->r_[i_] - parent_->l_[i_]; }
+
+        iterator begin() { return parent_->input_.begin() + pos(); }
+        iterator end()   { return parent_->input_.begin() + pos() + length(); }
+        const_iterator begin() const { return parent_->input_.begin() + pos(); }
+        const_iterator end()   const { return parent_->input_.begin() + pos() + length(); }
+
+        substr(const core* parent, int i)
+            : parent_(parent), i_(i)
+        {}
+
+    private:
+        const core* parent_;
+        int i_;
+    };
+
+private:
+    template <class Value>
+    struct substring_iterator
+        : public boost::iterator_facade<
+            substring_iterator<Value>,
+            Value,
+            boost::random_access_traversal_tag,
+            Value,
+            int>
+    {
+        substring_iterator()
+            : parent_(0), i_(-1)
+        {}
+
+        substring_iterator(const core* parent, int i)
+            : parent_(parent), i_(i)
+        {}
+
+    private:
+        friend class boost::iterator_core_access;
+
+        void increment() { ++i_; }
+        void decrement() { --i_; }
+        void advance(int n) { i_ += n; }
+        int distance_to(const substring_iterator<Value>& other) const { return other.i_ - this->i_; }
+
+        bool equal(const substring_iterator<Value>& other) const {
+            return this->parent_ == other.parent_ && this->i_ == other.i_;
+        }
+
+        Value dereference() const {
+            return substr(parent_, i_);
+        }
+
+        const core* parent_;
+        int i_;
+    };
+
+public:
+    typedef substring_iterator<substr> iterator;
+    typedef substring_iterator<const substr> const_iterator;
+
+    core(const std::vector<Char>& input, const size_t alphabet_size)
+        : input_(input),
+          sa_(input.size()),
+          l_(input.size()),
+          r_(input.size()),
+          d_(input.size()),
+          node_to_parent_node_()
+    {
+        // suffix array
+        int err = esaxx(input_.begin(),
+                        sa_.begin(),
+                        l_.begin(), r_.begin(), d_.begin(),
+                        static_cast<index_type>(input_.size()),
+                        static_cast<index_type>(alphabet_size),
+                        num_nodes_);
+        if (err) throw std::runtime_error("saisxx failed to construct a suffix array.");
+
+        // Dummy node
+        // These values are designed so that they can be used as well as those
+        // of the other nodes.
+        l_[num_nodes_] = 0;
+        r_[num_nodes_] = 0;
+        d_[num_nodes_] = 0;
+
+        // node_to_parent_node[i]: ノードiの親ノードの番号（post-order）。
+        // suffix_to_parent_node[k]: 接尾辞input[k..$]に対応する葉ノードの、親ノードのpost-order順の番号。
+        node_to_parent_node_.resize(num_nodes_);
+        std::vector<index_type> suffix_to_parent_node(input.size() + 1);
+        suffix_to_parent_node[input.size()] = num_nodes_;  // 接尾辞input[$..$]
+        {
+            std::stack<index_type> stk;  // the top of the stack is a current parent node
+            stk.push(num_nodes_);  // put the dummy node, which will be the parent of the root node
+            index_type next_node = num_nodes_ - 1;  // a node to consider next
+            index_type i = input.size() - 1;  // a current suffix, the i-th suffix in the suffix array
+            // narrow the range [l, r) to find the immediate parent of the i-th node
+            while (next_node >= 0 && l_[next_node] <= i && i < r_[next_node]) {
+                node_to_parent_node_[next_node] = stk.top();
+                stk.push(next_node);
+                --next_node;
+            }
+            while (i >= 0) {
+                // widen the range [l, r) to find the lowest ancestor of the i-th node
+                while (!(l_[stk.top()] <= i && i < r_[stk.top()])) {
+                    stk.pop();
+                }
+                // narrow the range [l, r) to find the immediate parent of the i-th node
+                while (next_node >= 0 && l_[next_node] <= i && i < r_[next_node]) {
+                    node_to_parent_node_[next_node] = stk.top();
+                    stk.push(next_node);
+                    --next_node;
+                }
+                suffix_to_parent_node[i] = stk.top();
+                --i;
+            }
+        }
+
+        // suffix_link_
+        suffix_link_.resize(num_nodes_);
+        suffix_link_[num_nodes_ - 1] = num_nodes_;  // transfers to the dummy node.
+        for (int i = 0; i < num_nodes_ - 1; ++i) {
+            // ここではノードi（iはpost-orderでの番号）に対応する部分文字列substrを扱う。
+            const auto len_substr  = d_[i];
+            const auto pos_substr  = sa_[l_[i]];
+            // substrの先頭を1文字削ったsub-substrを考える。
+            const auto len_subsubstr = len_substr - 1;
+            // sub-substrに対応するノードを見つける。
+            // {内部,葉}ノードに対応する部分文字列から先頭の1文字を削って得られ
+            // る文字列には、必ず対応する{内部,葉}ノードが存在する。
+            auto j = suffix_to_parent_node[pos_substr + 1];  // 接尾辞input[(pos_substr + j)..$]に対応する葉ノードの親ノード
+            while (d_[j] > len_subsubstr) j = node_to_parent_node_[j];  // d[j] == len_subsubstr ならば、ノードjはsub-substrに対応するノード。
+            suffix_link_[i] = j;
+        }
+    }
+
+    iterator begin() { return iterator(this, 0); }
+    iterator end()   { return iterator(this, num_nodes_); }
+    const_iterator begin() const { return const_iterator(this, 0); }
+    const_iterator end()   const { return const_iterator(this, num_nodes_); }
+
+private:
+    const std::vector<Char>& input_;
+    std::vector<index_type> sa_;
+    std::vector<index_type>  l_;
+    std::vector<index_type>  r_;
+    std::vector<index_type>  d_;
+    index_type  num_nodes_;
+    std::vector<index_type> node_to_parent_node_;
+    std::vector<index_type> suffix_link_;
+};
+
+
+template <class Char, class Index>
 struct BranchingSubstrings {
     typedef Index index_type;
     struct substr {
